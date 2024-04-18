b42e68c0f4f6724aca5df8cee7029ec176a67eff
Adiar <datkaevadiar@gmail.com>
2024-04-18 17:26:23 +0500

    feat: debounce

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import axios from 'axios'
+import debounce from 'lodash.debounce'
 import { inject, reactive, ref, watch, onMounted } from 'vue'
 import CardList from '@/components/CardList.vue'
 
@@ -23,9 +24,9 @@ const onChangeSelect = (event) => {
   filters.sortBy = event.target.value
 }
 
-const onChangeSearchInput = (event) => {
+const onChangeSearchInput = debounce((event) => {
   filters.searchQuery = event.target.value
-}
+}, 300)
 
 const addToFavorite = async (item) => {
   try {
